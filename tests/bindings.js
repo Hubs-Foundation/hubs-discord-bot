4bf1fea56831ce7153b7e86a5ab564f93799cd79
Marshall Polaris <marshall@pol.rs>
2019-02-05 17:26:03 -0800

    Fix up tests

--- tests/bindings.js
+++ tests/bindings.js
@@ -4,12 +4,12 @@ var ChannelBindings = require('../src/bindings.js').ChannelBindings;
 test('Hubs URLs are detected in channel topics', function(t) {
   var bindings = new ChannelBindings(["foo", "bar.mozilla.com"]);
   // hub IDs must be exactly 7 characters and followed by an optional slug
-  t.deepEqual(bindings.getHub("https://foo/0123456/"), "0123456");
-  t.deepEqual(bindings.getHub("https://foo/fiddles/blah-blah-blah"), "fiddles");
+  t.deepEqual(bindings.getHub("https://foo/0123456/").id, "0123456");
+  t.deepEqual(bindings.getHub("https://foo/fiddles/blah-blah-blah").id, "fiddles");
   t.deepEqual(bindings.getHub("https://bar.mozilla.com/spoke"), null);
   t.deepEqual(bindings.getHub("https://bar.mozilla.com/s0mething/"), null);
   t.deepEqual(bindings.getHub("https://bar.mozilla.com/"), null);
-  t.deepEqual(bindings.getHub("https://bar.mozilla.com/d0gf00d"), "d0gf00d");
+  t.deepEqual(bindings.getHub("https://bar.mozilla.com/d0gf00d").id, "d0gf00d");
   t.deepEqual(bindings.getHub("https://foo.bar.mozilla.com/whatever/"), null);
   t.deepEqual(bindings.getHub("https://zombo.com/hmmmm"), null);
   t.deepEqual(bindings.getHub("https://foo/index.html"), null);
